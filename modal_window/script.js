b85624fcbe73b4303137dda13b416835aa172367
geoGitAm <71641416+geoGitAm@users.noreply.github.com>
2021-03-29 15:56:25 +0300

    Finish up Modal Window project.

--- modal_window/script.js
+++ modal_window/script.js
@@ -1,2 +1,38 @@
 'use strict';
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', ShowModal);
+}
+
+btnCloseModal.addEventListener('click', HideModal);
+overlay.addEventListener('click', HideModal);
+document.addEventListener('keydown', CloseModalEsc);
+
+function CloseModalEsc(event) {
+    if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+        HideModal();
+    }
+}
+
+function ShowModal() {
+    ClassListRemove(modal, 'hidden');
+    ClassListRemove(overlay, 'hidden');
+}
+
+function HideModal() {
+    ClassListAdd(modal, 'hidden');
+    ClassListAdd(overlay, 'hidden');
+}
+
+function ClassListRemove(htmlElement, targetClassOrClasses) {
+    htmlElement.classList.remove(targetClassOrClasses);
+}
+
+function ClassListAdd(htmlElement, targetClassOrClasses) {
+    htmlElement.classList.add(targetClassOrClasses);
+}
