335d63ba34f270d64285118f4d18b831c4a17c75
geoGitAm <71641416+geoGitAm@users.noreply.github.com>
2021-04-01 21:59:22 +0300

    Add Todo-List project.

---
+++ todo_list_react/todo-list/src/todoData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Vacuum the room",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Paint the wall",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Clean the kitchen",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Mop the floor",
+        completed: false
+    },
+    {
+        id: 5,
+        text: "Take out the trash",
+        completed: false
+    }
+]
+
+export default todosData
